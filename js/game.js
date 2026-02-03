4f348167b958247bf8375a0d41fbd630c089813d
Virgile Hamraoui <virgile.h64@gmail.com>
2026-02-03 11:27:26 +0100

    Commit : Finished game background

--- js/game.js
+++ js/game.js
@@ -4,8 +4,19 @@ const ctx = canvas.getContext("2d");
 const img = new Image();
 img.src = "../assets/textures/background/tile_floor.png";
 
-img.onload = () => {
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  drawBackground();
+}
+
+function drawBackground() {
   const pattern = ctx.createPattern(img, "repeat");
   ctx.fillStyle = pattern;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+img.onload = () => {
+  resizeCanvas();
+  window.addEventListener("resize", resizeCanvas);
 };
