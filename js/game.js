f9d3a5ff5988916564b88f3bd02275194ab18d1d
Virgile Hamraoui <virgile.h64@gmail.com>
2026-02-03 11:09:31 +0100

    Added basic files

---
+++ js/game.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+const img = new Image();
+img.src = "..//assets//textures//background//tile_floor.png";
+
+img.onload = () => {
+  const pattern = ctx.createPattern(img, "repeat");
+  ctx.fillStyle = pattern;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+};
