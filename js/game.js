d9636870774fe08fd7f5039c3127c15ba4c173f0
Virgile Hamraoui <virgile.h64@gmail.com>
2026-02-03 11:15:05 +0100

    Commit: tiling now works

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 const img = new Image();
-img.src = "..//assets//textures//background//tile_floor.png";
+img.src = "../assets/textures/background/tile_floor.png";
 
 img.onload = () => {
   const pattern = ctx.createPattern(img, "repeat");
